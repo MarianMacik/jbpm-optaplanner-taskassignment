b566eadc006b853ba88799566c257580ad0ba89a
Marián Macik <mmacik@redhat.com>
2016-12-28 13:04:56 +0100

    Add benchmark test

---
+++ src/test/java/org/kie/demo/taskassignment/benchmark/BenchmarkTest.java
@@ -0,0 +1,19 @@
+package org.kie.demo.taskassignment.benchmark;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.optaplanner.benchmark.api.PlannerBenchmark;
+import org.optaplanner.benchmark.api.PlannerBenchmarkFactory;
+
+public class BenchmarkTest {
+
+    private static final String BENCHMARK_CONFIG_FILE = "org/kie/demo/taskassignment/benchmark/TaskAssigningBenchmarkConfig.xml";
+
+    @Test
+    public void runBenchmark() {
+        PlannerBenchmarkFactory plannerBenchmarkFactory = PlannerBenchmarkFactory.createFromXmlResource(BENCHMARK_CONFIG_FILE);
+        PlannerBenchmark plannerBenchmark = plannerBenchmarkFactory.buildPlannerBenchmark();
+        plannerBenchmark.benchmark();
+    }
+}
