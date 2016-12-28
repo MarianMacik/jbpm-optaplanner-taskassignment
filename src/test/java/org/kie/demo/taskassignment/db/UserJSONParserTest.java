0cb0a39367632a525a3a6031ad6f66eff90a0f2b
Marián Macik <mmacik@redhat.com>
2016-12-28 13:03:22 +0100

    Add UserJSONParserTest

---
+++ src/test/java/org/kie/demo/taskassignment/db/UserJSONParserTest.java
@@ -0,0 +1,63 @@
+package org.kie.demo.taskassignment.db;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.demo.taskassignment.planner.domain.SkillLevel;
+import org.kie.demo.taskassignment.test.util.AbstractCaseServicesBaseTest;
+import org.kie.demo.taskassignment.util.UserJSONParser;
+
+public class UserJSONParserTest extends AbstractCaseServicesBaseTest {
+
+    private EntityManager em;
+
+    @Before
+    public void setUp() throws Exception {
+        buildDatasource();
+        emf = Persistence.createEntityManagerFactory("org.jbpm.domain");
+        em = emf.createEntityManager();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        em.close();
+        if (emf != null) {
+            emf.close();
+        }
+        closeDataSource();
+    }
+
+    @Test
+    public void testUserJSONParser() {
+        UserJSONParser parser = new UserJSONParser(emf);
+        File file = new File("src/main/resources/org/kie/demo/taskassignment/db/testUsers.json");
+
+        parser.insertUsersFromFile(file);
+
+        List<UserEntity> users = em.createQuery("SELECT u from UserEntity u", UserEntity.class).getResultList();
+
+        Assertions.assertThat(users).hasSize(4);
+
+        users = users.stream().filter(userEntity -> userEntity.getName().equals("Mary")).collect(Collectors.toList());
+
+        UserEntity mary = users.get(0);
+
+        Assertions.assertThat(mary.getName()).isEqualTo("Mary");
+        Assertions.assertThat(mary.getUserSkills()).hasSize(2);
+        Map<String, SkillLevel> skills = mary.getUserSkills().stream().collect(Collectors.toMap(skillEntity -> skillEntity.getSkill().getName(), UserSkillEntity::getSkillLevel));
+        Assertions.assertThat(skills.get("management")).isEqualTo(SkillLevel.EXPERT);
+        Assertions.assertThat(skills.get("delivering")).isEqualTo(SkillLevel.BEGINNER);
+        Assertions.assertThat(mary.getUserGroups()).hasSize(1);
+        Assertions.assertThat(mary.getUserGroups().iterator().next().getGroup().getName()).isEqualTo("suppliers");
+    }
+}
